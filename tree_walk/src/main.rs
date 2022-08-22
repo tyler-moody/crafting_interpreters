e61ffb814b7e419e99305ec3694d4d372203b5fd
tmoody <spam@tylermoody.org>
2022-08-22 11:42:49 -0700

    argument parsing for tree walk interpreter

--- tree_walk/src/main.rs
+++ tree_walk/src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[clap(version)]
+struct Args {
+    /// Lox script to execute
+    script: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.script {
+        None => run_prompt(),
+        Some(script) => run_file(script),
+    }
+}
+
+fn run_file(script: PathBuf) {
+    println!(
+        "executing {}",
+        script.into_os_string().into_string().unwrap()
+    );
+}
+
+fn run_prompt() {
+    println!("prompt");
 }
