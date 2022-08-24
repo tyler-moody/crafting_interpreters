1e7fe15f5ee097b12447e711279a27f3d1a63291
tmoody <spam@tylermoody.org>
2022-08-24 07:53:09 -0700

    scan two-character operators

--- tree_walk/src/scanner.rs
+++ tree_walk/src/scanner.rs
@@ -17,7 +17,7 @@ pub enum TokenType {
 
     // one-or-two character tokens
     Bang,
-    BangBang,
+    BangEqual,
     Equal,
     EqualEqual,
     Greater,
@@ -98,6 +98,34 @@ impl Iterator for Source {
             Some('+') => Some(Ok(Token::new(TokenType::Plus, self.line))),
             Some(';') => Some(Ok(Token::new(TokenType::Semicolon, self.line))),
             Some('*') => Some(Ok(Token::new(TokenType::Star, self.line))),
+            Some('!') => match self.text.front() {
+                Some('=') => {
+                    self.text.pop_front();
+                    Some(Ok(Token::new(TokenType::BangEqual, self.line)))
+                }
+                _ => Some(Ok(Token::new(TokenType::Bang, self.line))),
+            },
+            Some('=') => match self.text.front() {
+                Some('=') => {
+                    self.text.pop_front();
+                    Some(Ok(Token::new(TokenType::EqualEqual, self.line)))
+                }
+                _ => Some(Ok(Token::new(TokenType::Equal, self.line))),
+            },
+            Some('>') => match self.text.front() {
+                Some('=') => {
+                    self.text.pop_front();
+                    Some(Ok(Token::new(TokenType::GreaterEqual, self.line)))
+                }
+                _ => Some(Ok(Token::new(TokenType::Greater, self.line))),
+            },
+            Some('<') => match self.text.front() {
+                Some('=') => {
+                    self.text.pop_front();
+                    Some(Ok(Token::new(TokenType::LessEqual, self.line)))
+                }
+                _ => Some(Ok(Token::new(TokenType::Less, self.line))),
+            },
             Some(c) => Some(Err(Error::BadChar { c, line: self.line })),
             None => {
                 if !self.eof_sent {
@@ -134,7 +162,7 @@ mod tests {
     }
 
     #[test]
-    fn test_single_character_literals() {
+    fn test_single_character_operators() {
         let source = "(){},.-+;*".to_string();
         let actual = scan_tokens(source);
         let expected = Ok(vec![
@@ -151,6 +179,69 @@ mod tests {
             Token::new(TokenType::EOF, 0),
         ]);
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_one_or_two_character_operators() {
+        assert_eq!(
+            Ok(vec![
+                Token::new(TokenType::Bang, 0),
+                Token::new(TokenType::EOF, 0)
+            ]),
+            scan_tokens("!".to_string())
+        );
+        assert_eq!(
+            Ok(vec![
+                Token::new(TokenType::BangEqual, 0),
+                Token::new(TokenType::EOF, 0)
+            ]),
+            scan_tokens("!=".to_string())
+        );
+
+        assert_eq!(
+            Ok(vec![
+                Token::new(TokenType::Equal, 0),
+                Token::new(TokenType::EOF, 0)
+            ]),
+            scan_tokens("=".to_string())
+        );
+        assert_eq!(
+            Ok(vec![
+                Token::new(TokenType::EqualEqual, 0),
+                Token::new(TokenType::EOF, 0)
+            ]),
+            scan_tokens("==".to_string())
+        );
+
+        assert_eq!(
+            Ok(vec![
+                Token::new(TokenType::Greater, 0),
+                Token::new(TokenType::EOF, 0)
+            ]),
+            scan_tokens(">".to_string())
+        );
+        assert_eq!(
+            Ok(vec![
+                Token::new(TokenType::GreaterEqual, 0),
+                Token::new(TokenType::EOF, 0)
+            ]),
+            scan_tokens(">=".to_string())
+        );
+
+        assert_eq!(
+            Ok(vec![
+                Token::new(TokenType::Less, 0),
+                Token::new(TokenType::EOF, 0)
+            ]),
+            scan_tokens("<".to_string())
+        );
+        assert_eq!(
+            Ok(vec![
+                Token::new(TokenType::LessEqual, 0),
+                Token::new(TokenType::EOF, 0)
+            ]),
+            scan_tokens("<=".to_string())
+        );
     }
 
     #[test]
