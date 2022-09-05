9de7be49bdd7bd64677859ef6ba3c393e233716d
tmoody <spam@tylermoody.org>
2022-09-04 23:26:29 -0700

    introduce the expression type, implement a simple expression pretty printer

---
+++ tree_walk/src/expression.rs
@@ -0,0 +1,140 @@
+use crate::scanner::Token;
+
+pub enum Expression<'a> {
+    Literal {
+        value: Box<dyn std::fmt::Display>,
+    },
+    Unary {
+        operator: Token,
+        expression: &'a Expression<'a>,
+    },
+    Binary {
+        left: &'a Expression<'a>,
+        operator: Token,
+        right: &'a Expression<'a>,
+    },
+    Grouping {
+        expression: &'a Expression<'a>,
+    },
+}
+
+pub fn print(expression: &Expression) -> String {
+    match expression {
+        Expression::Literal { value } => value.to_string(),
+        Expression::Unary {
+            operator,
+            expression,
+        } => parenthesize(&operator, &[expression]),
+        Expression::Binary {
+            left,
+            operator,
+            right,
+        } => parenthesize(&operator, &[left, right]),
+        Expression::Grouping { expression } => parenthesize(&"group".to_string(), &[expression]),
+    }
+}
+
+fn parenthesize(name: &impl std::fmt::Display, expressions: &[&Expression]) -> String {
+    let mut string = format!("({}", name);
+    for expression in expressions {
+        string = format!("{} {}", string, print(expression));
+    }
+    string.push(')');
+    string
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::scanner::TokenType;
+
+    #[test]
+    fn test_expression_instances() {
+        let foo = Expression::Literal {
+            value: Box::new("foo".to_string()),
+        };
+
+        let _unary = Expression::Unary {
+            operator: Token::new(TokenType::Minus, 0),
+            expression: &foo,
+        };
+
+        let bar = Expression::Literal {
+            value: Box::new("bar".to_string()),
+        };
+
+        let binary = Expression::Binary {
+            left: &foo,
+            operator: Token::new(TokenType::Plus, 0),
+            right: &bar,
+        };
+
+        let _grouping = Expression::Grouping {
+            expression: &binary,
+        };
+    }
+
+    #[test]
+    fn test_print_literal() {
+        let expected = "foo".to_string();
+        let literal = Expression::Literal {
+            value: Box::new(expected.clone()),
+        };
+
+        assert_eq!(expected, print(&literal));
+    }
+
+    #[test]
+    fn test_print_unary() {
+        let unary = Expression::Unary {
+            operator: Token::new(TokenType::Minus, 0),
+            expression: &Expression::Literal {
+                value: Box::new("foo".to_string()),
+            },
+        };
+        assert_eq!("(- foo)".to_string(), print(&unary));
+    }
+
+    #[test]
+    fn test_print_binary() {
+        let binary = Expression::Binary {
+            left: &Expression::Literal {
+                value: Box::new("foo".to_string()),
+            },
+            operator: Token::new(TokenType::Minus, 0),
+            right: &Expression::Literal {
+                value: Box::new("bar".to_string()),
+            },
+        };
+        assert_eq!("(- foo bar)", print(&binary));
+    }
+
+    #[test]
+    fn test_print_grouping() {
+        let grouping = Expression::Grouping {
+            expression: &Expression::Literal {
+                value: Box::new("foo".to_string()),
+            },
+        };
+        assert_eq!("(group foo)".to_string(), print(&grouping));
+    }
+
+    #[test]
+    fn test_example() {
+        let expression = Expression::Binary {
+            left: &Expression::Unary {
+                operator: Token::new(TokenType::Minus, 0),
+                expression: &Expression::Literal {
+                    value: Box::new(123),
+                },
+            },
+            operator: Token::new(TokenType::Star, 0),
+            right: &Expression::Grouping {
+                expression: &Expression::Literal {
+                    value: Box::new(45.67),
+                },
+            },
+        };
+        assert_eq!("(* (- 123) (group 45.67))".to_string(), print(&expression));
+    }
+}
--- tree_walk/src/scanner.rs
+++ tree_walk/src/scanner.rs
@@ -63,6 +63,52 @@ impl Token {
     }
 }
 
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.type_ {
+            TokenType::LeftParen => write!(f, "("),
+            TokenType::RightParen => write!(f, ")"),
+            TokenType::LeftBrace => write!(f, "{{"),
+            TokenType::RightBrace => write!(f, "}}"),
+            TokenType::Comma => write!(f, ","),
+            TokenType::Dot => write!(f, "."),
+            TokenType::Minus => write!(f, "-"),
+            TokenType::Plus => write!(f, "+"),
+            TokenType::Semicolon => write!(f, ";"),
+            TokenType::Slash => write!(f, "/"),
+            TokenType::Star => write!(f, "*"),
+            TokenType::Bang => write!(f, "!"),
+            TokenType::BangEqual => write!(f, "!="),
+            TokenType::Equal => write!(f, "="),
+            TokenType::EqualEqual => write!(f, "=="),
+            TokenType::Greater => write!(f, ">"),
+            TokenType::GreaterEqual => write!(f, ">="),
+            TokenType::Less => write!(f, "<"),
+            TokenType::LessEqual => write!(f, "<="),
+            TokenType::Identifier(string) => write!(f, "{}", string),
+            TokenType::Str(string) => write!(f, "{}", string),
+            TokenType::Number(number) => write!(f, "{}", number),
+            TokenType::And => write!(f, "and"),
+            TokenType::Class => write!(f, "class"),
+            TokenType::Else => write!(f, "else"),
+            TokenType::False => write!(f, "false"),
+            TokenType::Fun => write!(f, "fun"),
+            TokenType::For => write!(f, "for"),
+            TokenType::If => write!(f, "if"),
+            TokenType::Nil => write!(f, "nil"),
+            TokenType::Or => write!(f, "or"),
+            TokenType::Print => write!(f, "print"),
+            TokenType::Return => write!(f, "return"),
+            TokenType::Super => write!(f, "super"),
+            TokenType::This => write!(f, "this"),
+            TokenType::True => write!(f, "true"),
+            TokenType::Var => write!(f, "var"),
+            TokenType::While => write!(f, "while"),
+            TokenType::EOF => write!(f, "EOF"),
+        }
+    }
+}
+
 struct Source {
     text: VecDeque<char>,
     line: usize,
